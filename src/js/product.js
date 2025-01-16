f532ed5eb246be8d9080cf89a851d143a9a5860f
Cristobal Henriquez <brohenriquez@gmail.com>
2025-01-16 16:52:23 -0500

    commented out Wo1 cart fix (was interfering with team activity)

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,33 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParams } from './utils.mjs';
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParams('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart") || [];
-  if (!Array.isArray(cart)) {
-    cart = [cart];
-  }
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-  alert("Product added to cart:", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// function addProductToCart(product) {
+//   let cart = getLocalStorage("so-cart") || [];
+//   if (!Array.isArray(cart)) {
+//     cart = [cart];
+//   }
+
+//   cart.push(product);
+//   setLocalStorage("so-cart", cart);
+//   alert("Product added to cart:", product);
+// }
+
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
+
+
+
