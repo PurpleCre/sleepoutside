5593847644256b3f9503e8b54e257665dd30f6b8
PurpleCre <pgonese6@gmail.com>
2025-01-20 14:35:51 +0200

    fixed issue with vite

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -11,11 +11,6 @@ function productCardTemplate(product) {
         <h2 class="card__name">${product.Name}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
     `
-
-    // if (product.FinalPrice < product.SuggestedRetailPrice) {
-    //     const discount = product.SuggestedRetailPrice - product.FinalPrice;
-    //     html += `<p class="product-card__discount">Enjoy a $${discount.toFixed(2)} off discount!!!!</p>`
-    // }
     
     html += `</a>
         </li>
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-// import { loadHeaderFooter } from "./utils.mjs";
-// import Alert from "./Alert.mjs";
-
-// dynamically load header and footer
-// loadHeaderFooter();
-
-// alert logic
-// const mainEl = document.querySelector("main");
-// const alerts = new Alert(mainEl);
-// alerts.init();
 
 const data = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const list = new ProductListing("tents", data, element);
+const prodEl = document.querySelector(".product-list");
+const list = new ProductListing("tents", data, prodEl);
 
 list.init();
