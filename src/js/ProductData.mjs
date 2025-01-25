a8a8ee3df50a272dd54207261120cd8731ec22d0
Your Name <you@example.com>
2025-01-25 09:35:14 +0200

    initial commit

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -4,20 +4,3 @@ function convertToJson(res) {
   } else {
     throw new Error("Bad Response");
   }
-}
-
-export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
-  }
-}
