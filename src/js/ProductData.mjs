c344c422baf74e7f2b32b369fd01bd64c5825653
Cristobal Henriquez <brohenriquez@gmail.com>
2025-01-27 15:16:35 -0500

    restored previous working code

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -4,3 +4,20 @@ function convertToJson(res) {
   } else {
     throw new Error("Bad Response");
   }
+}
+
+export default class ProductData {
+  constructor(category) {
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
+  }
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
+  }
+  async findProductById(id) {
+    const products = await this.getData();
+    return products.find((item) => item.Id === id);
+  }
+}
