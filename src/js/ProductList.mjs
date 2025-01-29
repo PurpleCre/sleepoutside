1d52904a762cc4de7e5fa7144dee871a66fceda8
obylo98 <obinnaandrewonyekwere@gmail.com>
2025-01-29 15:18:06 +0100

    Product List sort

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -25,20 +25,40 @@ export default class ProductListing {
         this.dataSource = dataSource;
         this.listEl = listEl;
     }
+
     async init() {
-        // our dataSource will return a Promise...so we can use await to resolve it.
         const list = await this.dataSource.getData();
-        // filter the list to 4 items
-        const filteredList = this.filterList(list)
-        // render the list
-        this.renderList(filteredList);
+        this.fullList = list; // Store full dataset
+        this.filteredList = this.filterList([...this.fullList]); // Ensure a fresh copy
+        this.renderList(this.filteredList);
+
+        // Add event listener for sorting
+        document.getElementById("sort").addEventListener("change", (event) => {
+            this.sortList(event.target.value);
+        });
     }
+
     filterList(list, num = 4) {
         return list.slice(0, num);
-        // return list.filter((product) => product.Image)
     }
-    // render template lists
+
+    sortList(criteria) {
+        let sortedList = [...this.fullList]; // Always start fresh from full list
+
+        if (criteria === "name") {
+            sortedList.sort((a, b) => a.Name.localeCompare(b.Name));
+        } else if (criteria === "price") {
+            sortedList.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        }
+
+        // Reapply the filtering after sorting
+        this.filteredList = this.filterList(sortedList);
+        this.renderList(this.filteredList);
+    }
+
     renderList(list) {
+        this.listEl.innerHTML = ""; // Clear previous content to prevent duplication
         renderListWithTemplate(productCardTemplate, this.listEl, list);
     }
 }
+
