ed7413c9ed35f96041dddd104846f860b98e0bd6
Cristobal Henriquez <brohenriquez@gmail.com>
2025-01-30 17:20:32 -0500

    change name to new class (ExternalServices)

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
 import { getParams, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
 
 // get product ID
 const productId = getParams("product");
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
