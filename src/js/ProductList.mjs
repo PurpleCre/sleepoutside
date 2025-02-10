9afabb83081a003dce55f3f0e4eb0aca7fff10b0
obylo98 <obinnaandrewonyekwere@gmail.com>
2025-02-10 06:18:08 +0100

    fixed  breadcrumbs error, cart superscript error and api error

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -19,7 +19,7 @@ function productCardTemplate(product) {
   return html;
 }
 
-export default class ProductListing {
+export default class ProductList {
   constructor(category, dataSource, listEl) {
     this.category = category;
     this.dataSource = dataSource;
@@ -36,19 +36,27 @@ export default class ProductListing {
     return list;
   }
   async init() {
-    // our dataSource will return a Promise...so we can use await to resolve it.
-    const list = await this.dataSource.getData(this.category);
-    // filter the list to 4 items
-    const filteredList = this.filterList(list);
-    // render the list
-    this.renderList(filteredList);
+    try {
+      const list = await this.dataSource.getData(this.category);
 
-    // Sort the products/list by price or name
-    const sortElement = document.getElementById("sort");
-    sortElement.addEventListener("change", (event) => {
-      const sortedList = this.sortList(list, event.target.value);
-      this.renderList(sortedList);
-    });
+      // filter the list to 4 items
+      const filteredList = this.filterList(list);
+
+      // render the list
+      this.renderList(filteredList);
+
+      // Add breadcrumbs
+      this.handleBrandCrumbs(list);
+
+      // Sort the products/list by price or name
+      const sortElement = document.getElementById("sort");
+      sortElement.addEventListener("change", (event) => {
+        const sortedList = this.sortList(list, event.target.value);
+        this.renderList(sortedList);
+      });
+    } catch (error) {
+      console.error("Error in init:", error);
+    }
   }
   filterList(list, num = 4) {
     return list.slice(0, num);
@@ -60,8 +68,8 @@ export default class ProductListing {
   }
 
   // Add breadcrumbs to the page
-  handleBrandCrumbs() {
+  handleBrandCrumbs(list) {
     const breadcrumbsElement = document.querySelector("#breadcrumbs");
-    breadcrumbsElement.innerHTML = `<span class="path">${this.category}</span> <span class="arrow">></span><span class="path">(${this.products.length} items)</span>`;
+    breadcrumbsElement.innerHTML = `<span class="path">${this.category}</span> <span class="arrow">></span><span class="path">(${list.length} items)</span>`;
   }
 }
